39a54cc5730fd67745ed08172d7d950e7c57abbb
Pol Dok <iruekenrio@gmail.com>
2020-05-30 20:03:14 +0300

    maket complete

---
+++ lessons/28-05/1.js
@@ -0,0 +1,142 @@
+
+
+
+// console.log("Дарова"); 
+
+// alert("Привет");
+// confirm("Heeelllllooooo");  ok cancel
+// prompt("Test", "");  ok cancel input
+
+// "js динамически типизированный язык"
+
+// let tempWidth;
+// tempWidth = 12;
+
+// let tempHeight = 15;
+
+// console.log(tempHeight, tempWidth);
+
+// tempHeight = tempWidth;
+// console.log(tempHeight);
+
+// let a = 12, b = "String";
+// Infinity -Infinity NaN
+// console.log("asdf"/1);
+
+// let bigInt = 123213234523512355235235123514n
+
+//  string
+let name = "Boba"
+
+// let str = "Hi"
+// let str2 = 'Hello'
+// let str3 = `Hi ${name}, ${20 + 1}years`
+
+// console.log(str);
+// console.log(str2);
+// console.log(str3);
+
+// boolean bool
+let checkboxChecked = true;
+let radioChecked = false;
+
+// null , underfined
+let a = null;
+let b;
+// console.log(a, b)
+
+// object, symbol
+
+let ob = {
+    name: "Boba"
+}
+// typeof ob
+// typeof (ob)
+// console.log (typeof 12);
+// console.log (typeof 12n);
+// console.log (typeof true);
+// console.log (typeof "Hello");
+// console.log (typeof undefined);
+
+// console.log (typeof null);
+// console.log (typeof function(){});
+
+// console.log(`My name is ${name}`)
+// let name = "Boba";
+// унарный оператор
+let d = -2;
+
+// бинарный оператор
+let g = a + 4;
+
+let c = "My name is" + " " + "Boba";
+// console.log(3%2);
+
+let i = 0;
+
+// Инкремент
+// console.log("post",i++); 1.операция 2. +1
+// console.log("pref",++i); 1. +1 2.Операция
+
+
+// Декримент
+// i--
+// --i
+// console.log("post",i--);
+// console.log(i);
+// console.log("pref",--i);
+
+// a += b      a = a + b
+// a -= b      a = a - b
+// a *= b      a = a * b
+// a /= b      a = a / b
+// a %= b      a = a % b
+
+// let message = "Hi";
+// message += " Boba";
+
+let q = "2", f = 2;
+// console.log(q == f); 
+// console.log(q === f); строгое равенство
+// console.log(q < f);
+// console.log(q <= f);
+// console.log(q >= f);
+// console.log(q == f);
+// console.log(q != f);
+
+
+
+// console.log(1 + "2");
+// console.log("" + 1 + 0);
+// console.log("" - 1 + 0);
+// console.log("3" * "8");
+// console.log("px" + 1 + 14);
+// console.log(1 + 14 + "px");
+
+// console.log("42" - 1 + 14);
+// console.log("42px" - 1 + 14);
+// console.log(null + 1);
+// console.log(undefined + 1);
+
+// let value = true;
+// console.log(typeof value);
+// value = String(value);
+// console.log(typeof value);
+// console.log(value);
+
+// let value = "123asd";
+// console.log(typeof value);
+// value = +value;
+// value = Number (value);
+// console.log(typeof value);
+// console.log(value);
+
+// let value = "0";
+// console.log(typeof value);
+// value = Boolean (value);
+// console.log(typeof value);
+// console.log(value);
+
+
+
+
