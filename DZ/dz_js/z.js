ee52331338eb90b5b16e33b21f7b4d0e5445b255
Pol Dok <iruekenrio@gmail.com>
2020-06-22 16:41:57 +0300

    mini_dz

---
+++ DZ/dz_js/z.js
@@ -0,0 +1,14 @@
+let ch1 = document.getElementsByTagName('header')
+console.log(ch1);
+let ch2 = document.querySelectorAll('.nav .nav-item')
+console.log(ch2);
+let ch3 = document.getElementById('article')
+console.log(ch3);
+let ch4 = document.querySelector('[type = password]')
+console.log(ch4);
+let ch5 = document.getElementsByTagName('footer')
+console.log(ch5);
+let ch6 = document.querySelectorAll('footer ul')
+console.log(ch6);
+let ch7 = document.body.firstElementChild.nextSibling.nextElementSibling.firstElementChild.firstElementChild;
+console.log(ch7);
