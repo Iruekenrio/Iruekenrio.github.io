4938adbe1be7362cfe0dcb03dda3d15f530ae9c8
Pol Dok <iruekenrio@gmail.com>
2020-07-01 21:31:58 +0300

    'miniDz'

--- DZ/dz_js/z.js
+++ DZ/dz_js/z.js
@@ -1,14 +1,119 @@
-let ch1 = document.getElementsByTagName('header')
-console.log(ch1);
-let ch2 = document.querySelectorAll('.nav .nav-item')
-console.log(ch2);
-let ch3 = document.getElementById('article')
-console.log(ch3);
-let ch4 = document.querySelector('[type = password]')
-console.log(ch4);
-let ch5 = document.getElementsByTagName('footer')
-console.log(ch5);
-let ch6 = document.querySelectorAll('footer ul')
-console.log(ch6);
-let ch7 = document.body.firstElementChild.nextSibling.nextElementSibling.firstElementChild.firstElementChild;
-console.log(ch7);
+let input = document.querySelector('input');
+let list = document.querySelector('#list');
+input.addEventListener('keypress', function (e){
+    if(e.key == 'Enter'){
+        let li = document.createElement("li");
+        let task = document.createElement('span');
+        // console.log(e);
+        task.innerHTML = this.value;
+        li.append(task);
+        task.addEventListener('dblclick', function(){
+            newInput = document.createElement('input');
+            li.prepend(newInput);
+            // let plHolder = task.value
+            // newInput.innerHTML += task.innerHTML;
+            // console.log(newInput.value)
+            // newInput.setAttribute('placeholder', `${task.innerHtml}`)
+            // newInput.setAttribute('placeholder', `${newInput.value}`)
+            newInput.addEventListener('keypress', function (e){
+                if(e.key == 'Enter'){
+                    // console.log(this)                
+                    task.innerHTML = this.value;
+                    newInput.remove()
+                }
+            })
+            
+        })
+
+        let remove = document.createElement('span')
+        remove.classList.add('remove')
+        remove.innerHTML = ('remove')
+        li.append(remove)
+        remove.addEventListener('click', function(){
+            li.remove()
+        })
+        
+        let mark = document.createElement('span')
+        mark.classList.add('mark')
+        mark.innerHTML = ('mark')
+        li.append(mark)
+        mark.addEventListener('click', function(){
+            console.log(this)
+            this.classList.add('done')
+        })
+
+        list.append(li)
+
+        input.value = "";
+
+
+    }
+})
+
+
+
+
+// let table = document.createElement("table");
+// let tr = document.createElement("tr");
+// let td = document.createElement("td");
+
+// console.log(tr)
+// console.log(td)
+function tab (stroke, stb){
+    let table = document.createElement("table");
+    document.body.append(table)
+    for(let i = 0; i < stroke; i++){
+        let tr = document.createElement("tr");
+        tr.classList.add('box')
+        for(let i = 0; i < stb; i++){
+            let td = document.createElement("td");
+            tr.append(td)
+            td.classList.add('box')
+        }
+        table.append(tr)
+    }
+    
+}
+tab(5,5)
+let table = document.querySelector('table');
+let tr = document.querySelector('tr');
+let td = document.querySelectorAll('td');
+let box = document.querySelectorAll('box')
+// box.style.width = '100px';
+// console.log(document.querySelectorAll('td')[2])
+// console.log(document.querySelectorAll('td'))
+// document.querySelectorAll('td')[2].classList.add('red')
+
+function randomBackground () {
+    let arr = Array.from(document.querySelectorAll('td'))
+    // console.log(arr)
+    let redBack = []
+    console.log(redBack)
+    for(let i = 0; i < arr.length; i++){
+        console.log(i)
+        
+        function getRandom(){
+            return Math.random();
+        }
+        if(getRandom() >= 0.5){
+            document.querySelectorAll('td')[i].classList.add('red')
+            redBack.push(i)
+            
+        }else if(getRandom() <= 0.5){
+            0
+        }
+    }
+    let summ = document.createElement('div');
+    document.body.append(summ)
+    summ.innerHTML = `процент закрашеных: ${(redBack.length * 100) / arr.length}%`
+    // alert(`процент закрашеных: ${(redBack.length * 100) / arr.length}%`)
+}
+
+randomBackground()
+
+
+
+
+// let t = document.querySelectorAll('.red')
+// console.log(t)
+// console.log(document.querySelectorAll('.red'))
